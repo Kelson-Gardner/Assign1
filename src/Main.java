915691db26239e108eb46e9f541bf3e48089f2eb
kelson.gardner <kelson.gardner14@gmail.com>
2024-08-29 13:39:18 -0600

    Added help message for when no arguments are present

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if(args.length == 0) {
+            System.out.println("--- Assign 1 Help ---");
+            System.out.println("  -fib [n] : Compute the Fibonacci of [n]; valid range [0, 40]");
+            System.out.println("  -fac [n] : Compute the factorial of [n]; valid range, [0, 2147483647]");
+            System.out.println("  -e [n] : Compute the value of 'e' using [n] iterations; valid range [1, 2147483647]");
+        }
     }
 }
