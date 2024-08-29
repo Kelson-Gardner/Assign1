7736e44e94612ab78c700759b91497aa804477f8
kelson.gardner <kelson.gardner14@gmail.com>
2024-08-29 14:31:18 -0600

    Added factorial function and case in switch statement

--- src/Assign1.java
+++ src/Assign1.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 public class Assign1 {
     public static void printHelpMessage(){
         System.out.println("--- Assign 1 Help ---");
@@ -15,24 +17,46 @@ public class Assign1 {
         }
         return fibonacci(num - 1) + fibonacci(num - 2);
     }
+
+    public static BigInteger factorial(int num){
+        BigInteger product = BigInteger.ONE;
+        for(int i = 2; i <= num; i++){
+            product = product.multiply(BigInteger.valueOf(i));
+        }
+        return product;
+    }
     public static void main(String[] args) {
         if(args.length == 0 || args.length % 2 != 0) {
             printHelpMessage();
             return;
         }
         for(int i = 0; i < args.length; i += 2){
-            switch(args[i]){
-                case "-fib":
-                    int number = Integer.parseInt(args[i + 1]);
-                    if(number < 0 || number > 40){
-                        System.out.println("Fibonacci valid range is [0, 40]");
+            switch (args[i]) {
+                case "-fib" -> {
+                    try {
+                        int number = Integer.parseInt(args[i + 1]);
+                        if (number < 0 || number > 40) {
+                            System.out.println("Fibonacci valid range is [0, 40]");
+                        } else {
+                            System.out.println("Fibonacci of " + number + " is " + fibonacci(number));
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please enter a valid input for fibonacci");
                     }
-                    else {
-                        System.out.println("Fibonacci of " + number + " is " + fibonacci(number));
+                }
+                case "-fac" -> {
+                    try {
+                        int number = Integer.parseInt(args[i + 1]);
+                        if (number < 0) {
+                            System.out.println("Factorial valid range is [0, 2147483647]");
+                        } else {
+                            System.out.println("Factorial of " + number + " is " + factorial(number));
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please enter a valid input for factorial");
                     }
-                    break;
-                default:
-                    System.out.println("Unknown command line argument: " + args[i]);
+                }
+                default -> System.out.println("Unknown command line argument: " + args[i]);
             }
         }
     }
