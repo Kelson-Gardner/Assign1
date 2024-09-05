0d39c439e3c0e0e6b8d37b8dcdc3038c73332045
kelson.gardner <kelson.gardner14@gmail.com>
2024-09-04 21:38:17 -0600

    Added documentation for the ActionStrategy interface

--- src/ActionStrategy.java
+++ src/ActionStrategy.java
@@ -1,3 +1,11 @@
+/**
+ * @author Kelson Gardner
+ * Interface for strategy pattern
+ */
 public interface ActionStrategy {
+    /**
+     * Interface method for strategy pattern
+     * @param input input from the user
+     */
     void execute(String input);
 }
